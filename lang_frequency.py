5bcfc68d19348bfdcd010fedca26ac7a15d1cf7d
Ivan <ivan.dubrovskiy@gmail.com>
2019-03-21 16:03:31 +0300

    Added ability to read file from given path

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,7 +1,14 @@
+import os
 
 
-def load_data(filepath):
-    pass
+def load_data(file_path):
+    if not os.path.exists(file_path):
+        return None
+    with open(file_path, 'r', encoding='utf8') as file:
+        try:
+            return file.read()
+        except:
+            None
 
 
 def get_most_frequent_words(text):
