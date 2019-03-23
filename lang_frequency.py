78443f1c26ccaf2cfbb66d8eda9aad1930267abd
Ivan <ivan.dubrovskiy@gmail.com>
2019-03-23 23:21:47 +0300

    Add ability to specify path to the file

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,6 +2,8 @@ import os
 import re
 import logging
 import logging.config
+import argparse
+
 
 logging.config.fileConfig("logging.conf")
 
@@ -24,5 +26,22 @@ def get_most_frequent_words(tokenized_word_list):
     pass
 
 
+def get_console_args():
+    parser = argparse.ArgumentParser(
+        prog='Words frequency',
+        description='Get ten most frequent words from a given text.'
+    )
+
+    parser.add_argument(
+        '--file_path',
+        default='test.txt',
+        help='You need to specify the path to the text file from which'
+        'the script will count the frequency of the words'
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    pass
+    parse_args = get_console_args()
+    text = load_data(parse_args.file_path)
